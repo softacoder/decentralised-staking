d067204aaecae37d1b4e8565c3b0a367e28e258e
softacoder <113185323+softacoder@users.noreply.github.com>
2024-03-20 21:18:40 +0000

    commit install packages

--- challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
+++ challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -12,20 +12,6 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+// start at 12 min. my localhost does not dislpay. change from yarn to npm?
 
-
-  // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
-
-
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-}
+  
